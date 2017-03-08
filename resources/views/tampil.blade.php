a05b6bbad2ffc6d82a043679d7484db5f53c036f
jaka karang tunggal <tunggal.jaka@gmail.com>
2017-03-08 15:40:42 +0700

    jaka

---
+++ resources/views/tampil.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.index')
+@section('content')
+
+<div class="section">
+	<div class="card-panel purple darken-3 white-text">Tutorial Laravel 5.2 dengan Materializecss</div>
+</div>
+
+<div class="section">
+	
+	<div class="row">
+		<div class="col s12">
+			<h5>{{ $tampilkan->judul }}</h5>
+
+            <div class="divider"></div>
+			<img src="{{ asset('image/'.$tampilkan->gambar)  }}" style="max-height:200px;max-width:200px;margin-top:10px;">
+
+            <p>{!! $tampilkan->isi !!}</p>
+                
+		</div>
+	</div>
+
+</div>
+
+@endsection
